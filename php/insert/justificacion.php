1d0e8934a7a00975b75087e81ba857f296097e01
JuanENP <juanjose_skorg@hotmail.com>
2020-03-25 10:04:56 -0600

    consultas_actualizadas

--- php/insert/justificacion.php
+++ php/insert/justificacion.php
@@ -57,7 +57,7 @@ session_start();
     require("../../Acceso/global.php");
 
     /*OBTENER LA QUINCENA ACTUAL EN LA QUE ESTAMOS*/
-    $sql5="SELECT id from quincena where validez=1";
+    $sql5="SELECT idquincena from quincena where validez=1";
     $query5=mysqli_query($con, $sql5) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
     $resul5=mysqli_fetch_array($query5);
     $quincena=$resul5[0];
@@ -71,13 +71,13 @@ session_start();
         $id_incidencia;//paara guardar el id de incidencia que me arroja sql
 
         //ver si existe esa incidencia
-        $sql="  SELECT a.numero_trabajador, a.nombre, a.apellido_paterno, a.apellido_materno,f.entrada,b.fecha_entrada,f.salida,b.fecha_salida , b.quincena_id, b.id,c.id,c.clave_id,c.descripcion, f.turno
+        $sql="SELECT a.numero_trabajador, a.nombre, a.apellido_paterno, a.apellido_materno,f.entrada,b.fecha_entrada,f.salida,b.fecha_salida , b.quincena_quincena, b.id,c.idincidencia,c.clave_incidencia_clave_incidencia,c.descripcion, f.idturno
         FROM trabajador a
-        INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_Numero_trabajador and a.numero_trabajador='$num' and Cast(fecha_entrada As Date) ='$fecha'
-        INNER JOIN quincena  x on b.quincena_id = x.id  and  b.quincena_id = 5
-        INNER JOIN incidencia c on  b.id = c.asistencia_id  and (c.clave_id = 01 or c.clave_id = 02 or c.clave_id = 03) 
-        INNER JOIN acceso e on a.acceso_idacceso = e.idacceso 
-        INNER JOIN turno f on e.turno_turno = f.turno";
+        INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_trabajador and a.numero_trabajador='$num' and Cast(fecha_entrada As Date) ='$fecha'
+        INNER JOIN quincena  x on b.quincena_quincena = x.idquincena  and  b.quincena_quincena = 5
+        INNER JOIN incidencia c on  b.id = c.asistencia_asistencia  and (c.clave_incidencia_clave_incidencia = 01 or c.clave_incidencia_clave_incidencia = 02 or c.clave_incidencia_clave_incidencia = 03) 
+        INNER JOIN acceso e on a.numero_trabajador=e.trabajador_trabajador
+        INNER JOIN turno f on e.turno_turno = f.idturno";
 
         $query= mysqli_query($con, $sql) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
         //obtener las filas del query
@@ -97,26 +97,26 @@ session_start();
             /*Si el query tiene datos
             Ver si esa clave ya está justificada
             */
-            $sql2="SELECT a.numero_trabajador, a.nombre, a.apellido_paterno, a.apellido_materno,f.entrada,b.fecha_entrada,f.salida,b.fecha_salida , b.quincena_id,c.clave_id,d.clave_justificacion_id, f.turno,b.id,c.asistencia_id, c.id,d.incidencia_id
+            $sql2="SELECT a.numero_trabajador, a.nombre, a.apellido_paterno, a.apellido_materno,f.entrada,b.fecha_entrada,f.salida,b.fecha_salida , b.quincena_quincena,c.clave_incidencia_clave_incidencia,d.clave_justificacion_clave_justificacion, f.idturno,b.id,c.asistencia_asistencia, c.idincidencia,d.incidencia_incidencia
             FROM trabajador a
-            INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_Numero_trabajador and a.numero_trabajador = '$num' and b.id=$id_asistencia
-            INNER JOIN incidencia c on  b.id = c.asistencia_id and b.quincena_id = 5
-            INNER JOIN justificacion d on c.id = d.incidencia_id and d.clave_justificacion_id= 09
-            INNER JOIN acceso e on a.acceso_idacceso = e.idacceso
-            INNER JOIN turno f on e.turno_turno = f.turno";
+            INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_trabajador and a.numero_trabajador = '$num' and b.id=$id_asistencia
+            INNER JOIN incidencia c on  b.id = c.asistencia_asistencia and b.quincena_quincena = 5
+            INNER JOIN justificacion d on c.idincidencia = d.incidencia_incidencia and d.clave_justificacion_clave_justificacion= 09
+            INNER JOIN acceso e on a.numero_trabajador=e.trabajador_trabajador
+            INNER JOIN turno f on e.turno_turno = f.idturno";
             $query2= mysqli_query($con, $sql2) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
             //obtener las filas del query2
             $filas2= mysqli_num_rows($query2);
             if($filas2==0)
             {
                 //contamos cuántas 09 posee el empleado en la tabla justificaciones
-                $sql3="SELECT count(d.clave_justificacion_id)
+                $sql3="SELECT count(d.clave_justificacion_clave_justificacion)
                 FROM trabajador a
-                INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_Numero_trabajador and a.numero_trabajador = '$num' 
-                INNER JOIN incidencia c on  b.id = c.asistencia_id and b.quincena_id = 5
-                INNER JOIN justificacion d on c.id = d.incidencia_id and d.clave_justificacion_id= 09
-                INNER JOIN acceso e on a.acceso_idacceso = e.idacceso
-                INNER JOIN turno f on e.turno_turno = f.turno";
+                INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_trabajador and a.numero_trabajador = '$num' 
+                INNER JOIN incidencia c on  b.id = c.asistencia_asistencia and b.quincena_quincena = 5
+                INNER JOIN justificacion d on c.idincidencia = d.incidencia_incidencia and d.clave_justificacion_clave_justificacion= 09
+                INNER JOIN acceso e on a.numero_trabajador=e.trabajador_trabajador
+                INNER JOIN turno f on e.turno_turno = f.idturno";
                 $query3= mysqli_query($con, $sql3) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
                 $resul3=mysqli_fetch_array($query3);
                 $total=$resul3[0];
@@ -169,18 +169,18 @@ session_start();
         total=2
         */
         //contamos cuántas 09 (retardos justificados) posee el empleado en la tabla justificaciones
-        $sql6="SELECT count(d.clave_justificacion_id)
+        $sql6="SELECT count(d.clave_justificacion_clave_justificacion)
         FROM trabajador a
-        INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_Numero_trabajador and a.numero_trabajador = '$num' 
-        INNER JOIN incidencia c on  b.id = c.asistencia_id and b.quincena_id = 5
-        INNER JOIN justificacion d on c.id = d.incidencia_id and d.clave_justificacion_id= 09
-        INNER JOIN acceso e on a.acceso_idacceso = e.idacceso
-        INNER JOIN turno f on e.turno_turno = f.turno";
+        INNER JOIN asistencia b on a.numero_trabajador = b.trabajador_trabajador and a.numero_trabajador = '$num' 
+        INNER JOIN incidencia c on  b.id = c.asistencia_asistencia and b.quincena_quincena = 5
+        INNER JOIN justificacion d on c.idincidencia = d.incidencia_incidencia and d.clave_justificacion_clave_justificacion= 09
+        INNER JOIN acceso e on a.numero_trabajador=e.trabajador_trabajador
+        INNER JOIN turno f on e.turno_turno = f.idturno";
         $query6= mysqli_query($con, $sql6) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
         $resul6=mysqli_fetch_array($query6);
         $totalRetardos=$resul6[0];
 
-        //contamos cuántas 08 (omisiones justificadas) posee el empleado en la tabla justificaciones
+        //contamos cuántas 08 (omisiones justificadas) posee el empleado en la tabla justificaciones de incidencia y justificaciones de omisión
         
     }//FIN DEL IF OMISIÓN
 
@@ -205,7 +205,7 @@ session_start();
         $date2= new DateTime($fechaf);
         //echo $num . ". feini: " . $fecha . ". fechafin: " . $fechaf . ". hora en: " . $hora_e . ". hora sal: " . $hora_s;
         /*Ver si ese empleado ya posee una comisión*/
-        $sql7="SELECT * from especial where trabajador_numero_trabajador=$num and validez=1 and clave_especial_id=89";
+        $sql7="SELECT * from especial where trabajador_trabajador=$num and validez=1 and clave_especial_clave_especial=89";
         $query7=mysqli_query($con, $sql7) or die("<br>" . "Error: " . utf8_encode(mysqli_errno($con)) . " : " . utf8_encode(mysqli_error($con)));
         $resul7=mysqli_fetch_array($query7);
         $filas7= mysqli_num_rows($query7);
